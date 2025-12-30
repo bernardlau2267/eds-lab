7d7cc5f39450c3ad5ddc103b27633c5ef5824b9f
Bernard Lau <bernard_liu@outlook.com>
2025-12-30 17:34:06 +0800

    update styles and stuff

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -150,7 +150,7 @@ export default async function decorate(block) {
   const hamburger = document.createElement('div');
   hamburger.classList.add('nav-hamburger');
   hamburger.innerHTML = `<button type="button" aria-controls="nav" aria-label="Open navigation">
-      <span class="nav-hamburger-icon"></span>
+      <span class="nav-hamburger-icon nav-hamburger-icon-debug"></span>
     </button>`;
   hamburger.addEventListener('click', () => toggleMenu(nav, navSections));
   nav.prepend(hamburger);
